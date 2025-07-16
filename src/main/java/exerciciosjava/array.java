70f3e66dfcdd4dc9ae0b0aa692775793e5903023
jotap136 <joaopcsantos136@gmail.com>
2025-07-15 21:11:35 -0300

    teste com arraylist

---
+++ src/main/java/exerciciosjava/array.java
@@ -0,0 +1,25 @@
+package exerciciosjava;
+import java.util.ArrayList;
+
+public class array {
+ public static void main(String[] args) {
+	    /**String[] nome = new String [3];
+	    nome[0] = "Teste";
+	    nome[1] = "João";
+	    nome[2] = "Locaweb";
+		for (int i = 0; i < nome.size(); i++){
+	    //System.out.println(nome[0]);
+ 		}
+		**/
+		
+	    ArrayList<String> nome = new ArrayList<>();
+		nome.add("Paulo");
+		nome.add("Pedro");
+		nome.add("Pateta");
+		nome.add("aaaaaaaaaaa");
+		nome.remove(0);
+			System.out.println(nome.get(2));
+	    
+	}
+   
+}
